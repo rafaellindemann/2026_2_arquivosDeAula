892b12f90172551d736d19fc800c7d238c3ea7f9
rafaellindemann <rafaelld@gmail.com>
2026-09-01 20:30:48 -0300

    add 02-js

---
+++ 1a/02-JS/script.js
@@ -0,0 +1,31 @@
+// let nomeUsuario = 'Umberto Doisberto'
+
+function calcularLucroGanso(){
+    // entendimento
+        // O programa deve ler gastos, faturamento em ingressos e faturamento na loja e calcular o lucro em reais e o lucro percentual.
+
+    // infos e variáveis
+    let gastos, faturamentoIngressos, faturamentoLoja
+    let lucroReais, lucroPercentual
+
+    // entrada de dados
+    gastos = Number(prompt('Digite o gasto de hoje:'))
+        // gastos = Number('100')
+        // gastos = 100
+    faturamentoIngressos = Number(prompt('Faturamento em ingressos:'))
+    faturamentoLoja = Number(prompt('Faturamento na loja de itens:'))
+
+    // processamento
+    let faturamentoTotal = faturamentoIngressos + faturamentoLoja 
+    lucroReais = faturamentoTotal - gastos
+    
+    // gastos       --  100%
+    // lucroReais   --  x% (lucroPercentual)
+
+    // gastos*lucroPercentual = lucroReais*100
+    // lucroPercentual = (lucroReais*100)/gastos
+    lucroPercentual = (lucroReais/gastos)*100
+
+    // saída
+    alert('Lucro: R$' + lucroReais.toFixed(2) + ' ou ' + lucroPercentual + '%')
+}
