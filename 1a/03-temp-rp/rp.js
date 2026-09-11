6f6b92da737bc9ef142bd86efce991cfc81ce93b
rafaellindemann <rafaelld@gmail.com>
2026-09-10 22:05:33 -0300

    add rp

---
+++ 1a/03-temp-rp/rp.js
@@ -0,0 +1,73 @@
+
+let nota = 50
+
+if(nota >= 60){
+    alert()
+}else (nota < 60){;
+    alert()
+}
+
+if(nota >= 60){
+    alert()
+}else 
+    (nota < 60);
+{
+    alert()
+}
+
+if(nota>=60){
+    alert('tabom')
+}
+alert('tabom mesmo')
+
+if(nota>=60){
+    alert('tabom')
+    alert('tabom mesmo')
+}
+
+if(nota>=60) alert('parabens')
+
+
+// if(polvora > 80 && temperatura > 35){
+//     alert("Alerta vermelho")
+// }else{
+//     alert('Níveis seguros')
+// }
+
+// if(polvora > 80){
+//     if(temperatura > 35){
+//         alert("Alerta vermelho")
+//     }else{
+//         alert("Monitorar")
+//     }
+// }else{
+//     alert('Níveis seguros')
+// }
+
+
+// pts = maca * 10
+// pts = laranja * 5
+// pts = pessego * 7
+
+
+// pts = maca * 10
+// pts += laranja * 5
+// pts += pessego * 7
+
+// pts = maca * 10 + laranja * 5 + pessego * 7
+
+// idade = 18
+
+// idade = 1
+
+
+
+// if(condicao){
+//     // bloco
+// }
+
+// let i = 1
+// while(i<10){
+//     // bloco
+    
+// }
