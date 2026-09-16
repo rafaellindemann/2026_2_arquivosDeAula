59d73dbb67cea6bf184ab0108ae833925f7d65c4
rafaellindemann <rafaelld@gmail.com>
2026-09-15 22:01:19 -0300

    add repet00

---
+++ 1a/03-repeticoes/script.js
@@ -0,0 +1,35 @@
+
+
+// let contagem = 0
+// while(contagem < 3){
+//     alert(contagem)
+//     contagem++
+// }
+// let contagem = 4
+// while(contagem > 0){
+//     alert(contagem)
+//     contagem--
+// }
+// let contagem = 4
+// while(contagem > 0){
+//     contagem--
+//     alert(contagem)
+// }
+
+
+// let i = 10
+// while(i>3){
+//     if(i==6){
+//         alert(i)
+//     }
+//     console.log(i);
+//     i--
+// }
+
+
+let i = 1500
+while(i>0){
+    i--
+    if(i<1499)i=0
+    console.log(i+2);
+}
